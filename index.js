e57ee76ebd0dcc05324676865c67016c32c2510e
abi <abilalu1994@gmail.com>
2022-02-22 12:11:40 -0600

    updating js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '12787@Abi',
+    password: '12345',
     database: 'Employee_Tracker',
 });
 
